241184b90bb0b60f30e3a89872b96cb1fdc466a0
puebdev <pueblovasquez@hotmail.com>
2026-04-25 22:09:23 +0200

    fusions.js

---
+++ src/data/fusions.js
@@ -0,0 +1,28 @@
+export const fusions = [
+
+  // ROI GLUANT
+  { result:"Roi Gluant",recipe:["Gigluant","Gigluant"] },
+  { result:"Roi Gluant",recipe:["Gluant","Gluant","Gluant","Gluant"] },
+
+  // ROI GLUANT METAL
+  { result:"Roi Gluant de métal",recipe:[
+    "Gluant de mercure",
+    "Gluant de mercure",
+    "Gluant de mercure",
+    "Gluant de mercure"
+  ]},
+
+  // ARGON
+  { result:"Lézard Argon",recipe:["Komodor","Faune"] },
+
+  { result:"Grand Lézard Argon",recipe:[
+    "Lézard Argon","Lézard Argon","Lézard Argon","Lézard Argon"
+  ]},
+
+  { result:"Frou-frou",recipe:[
+    "Lézard Argon","Lézard Argon","Lézard Argon","Grand Lézard Argon"
+  ]},
+
+  { result:"Fri-Fri",recipe:["Komodor","Vampivol"] }
+
+];
