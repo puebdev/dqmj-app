6a9e62b4b892d777e44e688bbc9c55cf33331665
puebdev <pueblovasquez@hotmail.com>
2026-04-25 22:04:13 +0200

    monsters.js

---
+++ src/data/monsters.js
@@ -0,0 +1,12 @@
+export const monsters = [
+  { name: "Abyssaure", family: "Dragon", rank: "B" },
+  { name: "Aligatueur", family: "Naturel", rank: "A" },
+  { name: "Ame en peine", family: "Zombie", rank: "A" },
+  { name: "Aquasaure", family: "Dragon", rank: "D" },
+  { name: "Archer Vert", family: "Bête", rank: "C" },
+  { name: "Archidémon", family: "Démon", rank: "B" },
+  { name: "Armure Errante", family: "Zombie", rank: "C" },
+  { name: "Armure fatale", family: "Démon", rank: "B" },
+
+  // 👉 continue avec TOUTE ta liste (copier/coller)
+];
